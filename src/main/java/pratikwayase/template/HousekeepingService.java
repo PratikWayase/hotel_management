b4935b061a990fe97870e40000ab95f9c33ff7aa
pratik wayase <128498128+PratikWayase@users.noreply.github.com>
2025-05-30 19:34:28 +0530

    fi

--- src/main/java/pratikwayase/template/HousekeepingService.java
+++ src/main/java/pratikwayase/template/HousekeepingService.java
@@ -6,16 +6,16 @@ import pratikwayase.model.Room;
 public class HousekeepingService extends RoomServiceTemplate {
     @Override
     protected void checkServiceAvailability() {
-        System.out.println("Checking availability for housekeeping service (e.g., staff, time slots).");
+        System.out.println("Checking availability for housekeeping service.");
     }
 
     @Override
     protected void performService(Room room) {
-        System.out.println("Performing housekeeping for room: " + room.getRoomNumber() + " (cleaning, tidying).");
+        System.out.println("Performing housekeeping for room: " + room.getRoomNumber());
     }
 
     @Override
     protected void addServiceCharge(Room room) {
-        System.out.println("Adding housekeeping charge to room: " + room.getRoomNumber() + " (if applicable).");
+        System.out.println("Adding housekeeping charge to room: " + room.getRoomNumber());
     }
 }
