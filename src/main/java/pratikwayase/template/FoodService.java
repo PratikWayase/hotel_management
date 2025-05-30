59d8bf6c63cc1521502d65a796040d1065681a95
pratik wayase <128498128+PratikWayase@users.noreply.github.com>
2025-05-30 19:34:58 +0530

    Update FoodService.java

--- src/main/java/pratikwayase/template/FoodService.java
+++ src/main/java/pratikwayase/template/FoodService.java
@@ -7,16 +7,16 @@ import pratikwayase.model.Room;
 public class FoodService extends RoomServiceTemplate {
     @Override
     protected void checkServiceAvailability() {
-        System.out.println("Checking availability for food service (e.g., kitchen status, menu items).");
+        System.out.println("Checking availability for food service");
     }
 
     @Override
     protected void performService(Room room) {
-        System.out.println("Serving food to room: " + room.getRoomNumber() + " (delivery of order).");
+        System.out.println("Serving food to room: " + room.getRoomNumber());
     }
 
     @Override
     protected void addServiceCharge(Room room) {
-        System.out.println("Adding food service charge to room: " + room.getRoomNumber() + " (for items ordered).");
+        System.out.println("Adding food service charge to room: " + room.getRoomNumber());
     }
 }
